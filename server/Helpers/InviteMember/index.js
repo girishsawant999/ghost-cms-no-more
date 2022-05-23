d02cd1c46043c8571366b77bf21cf3a7115b7037
Girish Sawant <girishsawant999.gs@gmail.com>
2022-05-23 20:15:52 +0530

    feat: :sparkles: added cove comments implementation and adding member to contributor

---
+++ server/Helpers/InviteMember/index.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+const inviteMember = async ({ cookie, email }) => {
+  const url = `${process.env.SITE_URL}ghost/api/canary/admin/invites/`;
+  const headers = {
+    cookie,
+  };
+  const payload = {
+    invites: [
+      {
+        token: null,
+        email,
+        expires: null,
+        status: true,
+        role_id: "61e24cb56979ca6a2d7ed25d",
+      },
+    ],
+  };
+  const res = await axios.post(url, payload, { headers });
+  return res.data;
+};
+
+module.exports = inviteMember;
---
+++ server/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const inviteMember = require("./Helpers/InviteMember");
+
+const app = express();
+const port = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const createSession = async () => {
+  const username = process.env.USER_NAME;
+  const password = process.env.USER_PASSWORD;
+  console.log("createSession");
+  const url = `${process.env.SITE_URL}ghost/api/canary/admin/session`;
+  const res = await axios.post(url, { username, password });
+  return res.headers["set-cookie"];
+};
+
+app.get("/", (req, res) => {
+  res.send("Server is Active");
+});
+
+app.post("/member-added", async (req, res) => {
+  console.log("POST /member-added");
+  try {
+    const {
+      member: {
+        current: { email },
+      },
+    } = req.body;
+
+    const cookie = await createSession();
+    const response = await inviteMember({ cookie, email });
+    console.log("response", response);
+    return res.send(response);
+  } catch (error) {
+    console.log("error", error);
+    return res.send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
